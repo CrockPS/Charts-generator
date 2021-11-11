c26e02444b3bede5a5e8ddd6fbd5c2623d6ba955
Cássio Ribeiro <crcassioribeiro@gmail.com>
2021-11-10 22:03:32 -0300

    segundo envio

--- src/Components/Charts.js
+++ src/Components/Charts.js
@@ -1,73 +1,39 @@
 import React from 'react';
 import Plot from 'react-plotly.js'
 
-/**
- * Create two arrays num length with random values from 0 to mul
- *
- * @param {*} num
- * @param {*} mul
- * @returns
- */
-function randomValues(num, mul) {
-  const arr = [];
-  const index = [];
-  for (let i = 0; i < num; i++) {
-    arr.push(Math.random() * mul)
-    index.push(i);
-  }
-  return {index, arr};
-}
 
-/**
- * Main application component
- *
- * @returns
- */
 const Charts = () => {
-  const traces = Array(3).fill(0).map((_, i) => {
-    const {index, arr} = randomValues(20, 3);
-    return {
-      x: Array(20).fill(i),
-      y: index,
-      z: arr,
-      type: 'scatter3d',
-      mode: 'lines'
-    }
-  });
-//[1, 1, 1, 1, 1, 1, 1, 1, 1],
+
+  function cinco(){
+    const contaInput = document.querySelector("#input1");
+    const pessoaInput = document.querySelector("#input3");
+    const conta = parseFloat(contaInput.value, 10);
+    const pessoa = parseFloat(pessoaInput.value, 10);
+
+    var tipAmount = conta * 0.05 / pessoa;
+    var total = tipAmount + conta / pessoa;
+
+    document.querySelector("#value1").innerHTML = "$"+parseFloat(tipAmount.toFixed(2));}
+
+  var x = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  var y = [20, 14, 25, 16, 18, 22, 19, 15, 12, 16, 14, 17]
+
+  var tipo = 'bar'
+
   return (
-    <>
+      <>
         <Plot
-        data={traces}
-            layout={{
-                width: 500,
-                height: 500,
-                title: `Simple 3D Scatter`
-            }}
+          data={[
+            {type: tipo, x: x, y: y},
+          ]}
+          layout={ {width: 500, height: 500, title: 'A Fancy Plot'} }
         />
-        <Plot
-        data={[
-          {
-            x: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            y: ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'setembro', 'outubro'],
-            z: [3, 4, 2, 6, 8, 2, 9, 3, 5],
-            type: 'scatter3d',
-            mode: 'lines',
-            marker: {color: 'red'},
-          },
-          {
-            x: [2, 2, 2, 2, 2, 2, 2, 2, 2],
-            y: ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'setembro', 'outubro'],
-            z: [5, 9, 4, 1, 8, 2, 4, 1, 3],
-            type: 'scatter3d',
-            mode: 'lines',
-            marker: {color: 'green'},
-          },
-        ]}
-        layout={ {width: 500, height: 500, title: 'A Fancy Plot'} }
-      />
-    </>
-  );
+
+        <input type="text" id="input1"/>
+        <input type="text" id="input1"/>
+        <p id="result">resultado</p>
+      </>
+    );
 }
 
 export default Charts;
