e0865d1e74289c0abc130c40f9270fbfd1b0c2c6
Cássio Ribeiro <crcassioribeiro@gmail.com>
2021-11-22 18:19:52 -0300

    quinto envio

--- src/Components/RegisterForm.js
+++ src/Components/RegisterForm.js
@@ -1,14 +1,25 @@
 import { ErrorMessage } from "@hookform/error-message";
 import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
+import Api from "./Api.js"
 import './LoginForm.css';
-
+//console.log({data});
 const RegisterForm = () => {
 
     const { handleSubmit, register, errors, watch } = useForm(); 
-    const onSubmit = (data) => console.log({data});
-    const password = useRef({});
-    password.current = watch("password", "");
+    //const password = useRef({});
+    //password.current = watch("password", "");
+    //const onSubmit = (data) => console.log({data});
+
+    const onSubmit = (data) => {
+        fetch('https://graphics-login-api.herokuapp.com/api/users', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+        .then(response => {console.log(response);})
+        .then(data => this.setState({ postId: data.id }));
+    }
 
         return (
         <>
@@ -17,8 +28,8 @@ const RegisterForm = () => {
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <label>
                         <p>Digite seu email:</p>
-                        <input id="input-focus" type="text" name="email" placeholder="exemplo@email.com"
-                        {...register('email', { required: "Email obrigatório!", pattern: 
+                        <input id="input-focus" type="text" name="login" placeholder="nome@email.com"
+                        {...register('login', { required: "Email obrigatório!", pattern: 
                         {value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/, message: "Digite um email válido!"} })}/>
                     </label>
                     {errors?.email && <ErrorMessage message={errors.email.message}/>}
@@ -28,13 +39,7 @@ const RegisterForm = () => {
                         {...register('password', { required: "A senha deve ter minimo seis caracteres!", minLength: 6 })}/>
                     </label>
                     {errors?.password && <ErrorMessage message={errors.password.message}/>}
-                    <label>
-                        <p>Confirme sua senha</p>
-                        <input id="input-focus" type="password" name="password_repeat" 
-                        {...register('password_repeat', {required: "Deve ser igual a senha que você já digitou!" , 
-                        validate: value =>value === password.current})}/>
-                    </label>
-                    {errors?.password_repeat && <ErrorMessage message={errors.password_repeat.message}/>}
+                    
                     <div>
                         <button id="submit-button" type="submit">Criar conta</button>
                     </div>
@@ -45,3 +50,22 @@ const RegisterForm = () => {
 }
 
 export default RegisterForm;
+
+/* CONFIRMAR SENHA
+                    <label>
+                        <p>Confirme sua senha</p>
+                        <input id="input-focus" type="password" name="password_repeat" 
+                        {...register('password_repeat', {required: "Deve ser igual a senha que você já digitou!" , 
+                        validate: value =>value === password.current})}/>
+                    </label>
+                    {errors?.password_repeat && <ErrorMessage message={errors.password_repeat.message}/>}
+ */
+
+ /*const onSubmit = (data) => {
+        fetch('https://graphics-login-api.herokuapp.com/api/users', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => this.setState({ postId: data.id }));
+    } */
